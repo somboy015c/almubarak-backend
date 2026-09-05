a3ce4a43f1d5c42e8117089d1d06ac7eee9c6414
Mahmud Shamsudeen <mahmudshamsudeen@gmail.com>
2026-09-05 14:45:26 +0100

    Add files via upload

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,19 @@
+const { v4: uuidv4 } = require('uuid');
 
+function generateRef(prefix = 'ALM') {
+  return `${prefix}-${Date.now()}-${uuidv4().slice(0, 8).toUpperCase()}`;
+}
+
+function applyMarkup(providerCost, markupPercent = 0) {
+  const cost = Number(providerCost);
+  const finalPrice = cost + cost * (Number(markupPercent) / 100);
+  return Math.round(finalPrice * 100) / 100;
+}
+
+function publicUser(user) {
+  if (!user) return null;
+  const { password, ...rest } = user;
+  return rest;
+}
+
+module.exports = { generateRef, applyMarkup, publicUser };
